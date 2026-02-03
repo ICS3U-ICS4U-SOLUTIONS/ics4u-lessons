81f3365101dcf3a840f7986aad80130fbd2b80ea
e28166 <e28166@FXH-9496082790>
2026-02-03 12:39:51 -0500

    s

---
+++ src/lessons/Temp.java
@@ -0,0 +1,27 @@
+package lessons;
+
+public class Temp {
+
+	public static void main(String[] args) {
+
+		// variables
+		int myInteger1 = 32;
+		int myInteger2 = 4;
+		
+		
+		System.out.println(addTwoIntegers(23, 4));
+		
+	}
+	
+	public static int addTwoIntegers(int x, int y)  {
+		
+		int answer = x + y;
+		
+		return answer;
+		
+	}
+	
+	
+	
+
+}
