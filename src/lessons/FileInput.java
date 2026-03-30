be194e06d47b0e68b9b2484337e3ad8d6b401268
e28166 <e28166@FXH-9496082790>
2026-03-30 13:35:58 -0400

    asdf

---
+++ src/lessons/FileInput.java
@@ -0,0 +1,57 @@
+package lessons;
+import java.util.ArrayList;
+import java.io.*;
+
+public class FileInput {
+
+	public static void main(String[] args) {
+
+		// LESSON - reading .txt file into an ArrayList
+		//			then convert ArrayList to an Array
+	
+		// variables
+		ArrayList<String> colours = new ArrayList();
+		BufferedReader br = null;
+		String word = "";
+		
+		try  {
+			
+			// open file
+			br = new BufferedReader(new FileReader("colours.txt"));
+			
+			// iterate through file (in the buffer) until we hit end of file (null)
+			// add each word into ArrayList
+			while ( (word = br.readLine()) != null ) {
+				
+				colours.add(word);
+			}
+			
+			// close the file (memory management)
+			br.close();
+		}
+		
+		catch (Exception e)  {
+			
+			e.printStackTrace();
+		}
+
+		// print ArrayList
+		for (int i=0; i<colours.size(); i++)  {
+			
+			System.out.println(colours.get(i));
+		}
+		
+		// create Array, move contents of ArrayList to Array, clear ArrayList
+		String[] myArray = new String[colours.size()];
+		colours.toArray(myArray);
+		colours.clear();
+		
+		// print Array
+		for(int i=0; i<myArray.length; i++)  {
+			
+			System.out.println(myArray[i]);
+		}
+			
+	}
+
+}
---
+++ src/lessons/FileOutput.java
@@ -0,0 +1,39 @@
+package lessons;
+import java.util.ArrayList;
+import java.io.*;
+
+public class FileOutput {
+
+	public static void main(String[] args) {
+
+		// variables
+		String[] food = {"milk", "bread", "butter", "cheese", "yogurt"};
+		
+		try  {
+			
+			// create file, buffer and file writer
+			FileOutputStream fout = new FileOutputStream("food.txt");
+			BufferedOutputStream bout = new BufferedOutputStream(fout);
+			OutputStreamWriter out = new OutputStreamWriter(bout);
+			
+			// write contents of Array to file
+			for(int i=0; i<food.length; i++)  {
+				
+				out.write(food[i] + "\n");
+			}
+			
+			// force data to be written
+			out.flush();
+			
+			// clear memory
+			out.close();
+		}
+
+		catch(Exception e)   {
+			
+			e.printStackTrace();
+		}
+		
+	}
+
+}
