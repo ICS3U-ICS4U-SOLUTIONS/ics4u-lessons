46897d4f6d54adb53bdb68c06c28ee2712042592
e28166 <e28166@FXH-9496082790>
2026-04-21 13:44:21 -0400

    asdf

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -2,14 +2,87 @@ package sorting;
 
 public class BubbleSort {
 
+	public static int numSwaps = 0;
+	public static int numComparisons = 0;
+	
 	public static void main(String[] args) {
 
+		// unsorted array
 		int[] myArray = {5, 2, 3, 1, 4, 9, 6, 7, 8, 0};
+
 		printArray(myArray);
-		swap(myArray, 2, 5);
+
+		swap(myArray, 2, 5); // test swap method
+
 		printArray(myArray);
+
+		bubbleSort(myArray, false);
+		
+		printArray(myArray);
+		
+		System.out.println("SWAPS: " + numSwaps);
+		System.out.println("COMPARISONS: " + numComparisons);
+		
 	}
 	
+	
+	/*	DESCRIPTION - Sorts int[] in ascending or descending order (true/false).
+	 * 	PARAMETERS - int[] a, boolean ascending
+	 *	RETURN TYPE - void
+	 */
+	public static void bubbleSort(int[] a, boolean order)  {
+		
+		boolean ascending = order;
+		boolean swapped = true;
+		
+		// sort ascending order
+		if (ascending == true)  {
+			
+			while(swapped == true)  {
+				
+				swapped = false;
+				
+				for(int i=0; i<a.length-1; i++)  {
+					
+					numComparisons++;
+					
+					if(a[i] > a[i+1])  {
+						
+						swap(a, i, i+1);
+						numSwaps++;
+						
+						swapped = true;
+					}
+				}
+			}
+		}
+		
+		
+		// sort descending order
+		if (ascending == false)  {
+					
+			while(swapped == true)  {
+						
+				swapped = false;
+						
+				for(int i=0; i<a.length-1; i++)  {
+							
+					numComparisons++;
+							
+					if(a[i] < a[i+1])  {
+								
+						swap(a, i, i+1);
+						numSwaps++;
+								
+						swapped = true;
+					}
+				}
+			}
+		}
+		
+		
+		
+	}
 	
 	/*	DESCRIPTION - Swaps int[x] with int[y].
 	 * 	PARAMETERS - int[] a, int x, int y
