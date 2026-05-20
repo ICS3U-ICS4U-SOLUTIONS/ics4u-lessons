cb5cef1e602a2db1eedf6b7cb6d31f61baccac2c
e28166 <e28166@FXH-9496082790>
2026-05-20 08:30:04 -0400

    sad

---
+++ src/sorting/MergeSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+
+	}
+	
+	
+	/*  DESCRIPTION - Sorts int[] in ascending order.
+	 *  PARAMETERS - int[] a
+	 *  RETURN - int[]
+	 */
+	public static int[] mergeSort(int[] a)  {
+		
+		int[] fubar = new int[0];
+		return fubar;
+	}
+		
+	
+	/*  DESCRIPTION - Merges int[] a with int[] b in ascending order.
+	 *  PARAMETERS - int[] a, int[] b
+	 *  RETURN - int[]
+	 */
+	public static int[] merge(int[] a, int[] b)  {
+		
+		int[] fubar = new int[0];
+		return fubar;
+	}
+}
