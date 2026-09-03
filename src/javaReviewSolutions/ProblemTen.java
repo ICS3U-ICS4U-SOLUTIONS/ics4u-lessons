38fb87903d53780df1f42d5665ecf073f2f2074a
e28166 <e28166@FXH-9496082790>
2026-09-03 12:16:48 -0400

    a

---
+++ src/javaReviewSolutions/ProblemTen.java
@@ -0,0 +1,11 @@
+package javaReviewSolutions;
+
+public class ProblemTen {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI);
+
+	}
+
+}
---
+++ src/javaReviewSolutions/ProblemTwenty.java
@@ -0,0 +1,12 @@
+package javaReviewSolutions;
+
+public class ProblemTwenty {
+
+	public static void main(String[] args) {
+
+
+		System.out.println(System.getProperty("java.version"));
+
+	}
+
+}
